bfc59b5e99563748c653632f5de064f74d0b9de8
연진주 <jinjuyeon3@gmail.com>
2026-02-20 23:28:20 +0900

    반복문 예제 복습

---
+++ ch30-iteration/js/ex1-self.js
@@ -0,0 +1,65 @@
+let cafeList = [
+    [
+        0,
+        "Basking Coffee",
+        "福岡市中央区六本松",
+        "8:00 ~ 19:00",
+        "@basking.ropponmatsu",
+        "https://www.instagram.com/basking.ropponmatsu",
+        "basking-coffee-ropponmatsu.jpg"
+    ],
+    [
+        1,
+        "Gnu Coffee",
+        "福岡市中央区今川",
+        "13:00 ~ 18:00",
+        "@gnu_coffee",
+        "https://www.instagram.com/gnu_coffee",
+        "gnu-coffee.jpg"
+    ],
+    [
+        2,
+        "NORD cafe",
+        "北九州市小倉北区足立",
+        "11:00 ~ 17:00",
+        "@nord_cafe_",
+        "https://www.instagram.com/nord_cafe_",
+        "nord-cafe.jpg"
+    ],
+    [
+        3,
+        "on the Toast",
+        "福岡市中央区六本松",
+        "11:30 ~ 17:00",
+        "@onthe_toast",
+        "https://www.instagram.com/onthe_toast",
+        "on-the-toast.jpg"
+    ],
+    [
+        4,
+        "Siro Coffee",
+        "福岡市早良区城西",
+        "12:30 ~ 18:00",
+        "@siro_coffee",
+        "https://www.instagram.com/onthe_toast",
+        "siro-coffee.jpg"
+    ],
+    [
+        5,
+        "そふ珈琲",
+        "福岡市城南区別府",
+        "13:00 ~ 18:00",
+        "@sofucoffee",
+        "https://www.instagram.com/sofucoffee",
+        "sofu-coffee.jpg"
+    ],
+    [
+        6,
+        "コネクトコーヒー",
+        "福岡市中央区天神",
+        "12:00 ~ 20:00",
+        "@connectcoffee.co",
+        "https://www.instagram.com/onnectcoffee.co",
+        "コネクトコーヒー.jpg"
+    ],
+]
