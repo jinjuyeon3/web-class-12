29a33aeb6e63b422b15eba19839e896d6ae181cb
연진주 <jinjuyeon3@gmail.com>
2026-03-30 16:19:26 +0900

    스크롤기능 공부1

---
+++ ch39-scroll/js/study1.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const header = document.querySelector("header")
+    const currentCount = document.querySelector("#current-count")
+    const rotateBox = document.querySelector(".rotate-box")
+    const sections = document.querySelectorAll(".box>section")
+
+    window.addEventListener("scroll",()=>{
+        // 스크롤바가 움직일 때 실행되는 소스코드
+        let distance = window.scrollY
+        currentCount.innerHTML = distance
+        rotateBox.style.transform = `rotate(${distance/5}deg)`
+        if(distance>=600){
+            header.classList.add("on")
+        }else{
+            header.classList.remove("on")
+        }
+        sections.forEach((section)=>{section.style.background = `url(./img/bg-wave2.png) repeat-x ${-distance/13}% 0%;`})
+        
+    })
+})
